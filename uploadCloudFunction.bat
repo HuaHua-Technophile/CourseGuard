901477cd395520556dcadb4e4c6d6348cb1fa142
V-I-G <3118590779@qq.com>
2023-06-25 18:54:16 +0800

    first commit

---
+++ uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Tencent\微信web开发者工具\cli.bat" cloud functions deploy --e cloud1-1gfryui8b6a1c41c --n quickstartFunctions --r --project "C:\Study\CourseGuard" --report_first --report
