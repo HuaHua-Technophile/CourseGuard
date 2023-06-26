05c67411bd225c1697b45b748a620f595e534daa
V-I-G <3118590779@qq.com>
2023-06-26 22:01:44 +0800

    提交

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -11,40 +11,44 @@ Page({
   onLoad(options) {
     const db = wx.cloud.database(); //在开始使用数据库 API 进行增删改查操作之前，需要先获取数据库的引用。以下调用获取默认环境的数据库的引用
     const Curriculum = db.collection("Curriculum");
+    Curriculum.where({
+      curriculumName: '粤嵌课表',
+    }).get({
+      success: function (res) {
+        console.log(res.data);
+      },
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {},
+  onReady() { },//生命周期函数--监听页面初次渲染完成
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow() {},
+  onShow() { },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide() {},
+  onHide() { },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload() {},
+  onUnload() { },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh() {},
+  onPullDownRefresh() { },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom() {},
+  onReachBottom() { },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage() {},
+  onShareAppMessage() { },
 });
