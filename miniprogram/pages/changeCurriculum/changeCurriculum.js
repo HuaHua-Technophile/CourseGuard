83ab04ced5b157bf0794dd0591d7e24fd0d06e5c
V-I-G <3118590779@qq.com>
2023-07-03 16:03:11 +0800

    同步

--- miniprogram/pages/changeCurriculum/changeCurriculum.js
+++ miniprogram/pages/changeCurriculum/changeCurriculum.js
@@ -5,14 +5,23 @@ Page({
    */
   data: {
     theme: "light",
+    CurriculumList: [],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-    // 接收其他页面传值
+    // 接收其他页面传值-------------------------
     this.setData({ theme: options.theme });
+    const db = wx.cloud.database(); //在开始使用数据库 API 进行增删改查操作之前，需要先获取数据库的引用。以下调用获取默认环境的数据库的引用
+    // 获取所有课表--------------------------
+    db.collection("Curriculum").get({
+      success:  (res)=> {
+        this.setData({ CurriculumList :res.data});
+        console.log('您的所有课程表:',this.data.CurriculumList);
+      },
+    });
   },
 
   /**
