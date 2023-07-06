601ec3afb81c2fb8014798a97d4ea08f7aa59eb8
V-I-G <3118590779@qq.com>
2023-07-06 10:48:50 +0800

    修复课程增删改时，无法正确同步数据库的BUG

--- miniprogram/pages/addCourse/addCourse.js
+++ miniprogram/pages/addCourse/addCourse.js
@@ -1,11 +1,9 @@
 const app = getApp();
 const db = wx.cloud.database().collection("Curriculum"); //在开始使用数据库 API 进行增删改查操作之前，需要先获取数据库的引用。以下调用获取默认环境的数据库的引用
-
 Page({
   data: {
-    id: -1, //传入的当前课表的id
+    Curriculum: {}, //课程表数据,在保存后使用set更新记录时,需要放入部分的原本数据
     CourseList: [], //所有课程的数据存放,因为页面的顺序不能乱,因此将对象改为数组
-    CourseListBackup: [], //页面进入时的数据备份,退出时检查一致性,若不一致则提示保存
     CourseIndex: "", // 指定当前缓冲栈中的颜色存入哪个课程
     color: "", //指定当前缓冲栈中的颜色存入课程的背景色还是前景色
     rgb: "rgb(0,154,97)", //当前存放进入缓冲栈的的颜色值
@@ -56,11 +54,13 @@ Page({
   },
   // 输入框失去焦点保存课程名称
   changeValue(e) {
+    console.log("即将编辑:", this.data.CourseList[e.target.id]);
     if (e.detail.value != "")
       this.setData({
         [`CourseList[${e.target.id}].${e.target.dataset.value}`]: e.detail
           .value,
       });
+    console.log("编辑完成:", this.data.CourseList);
   },
   saveCourse() {
     if (this.data.CourseList.some((i) => i.name == ""))
@@ -72,9 +72,13 @@ Page({
     this.data.CourseList.forEach((i) => {
       Course[i.name] = i;
     });
-    db.where({ _id: this.data.id }).update({
+    db.doc(`${app.globalData.id}`).set({
       data: {
-        Course: Course,
+        Course,
+        name: this.data.Curriculum.name,
+        classInfo: this.data.Curriculum.classInfo,
+        arrangement: this.data.Curriculum.arrangement,
+        hour: this.data.Curriculum.hour,
       },
       success: function (res) {
         console.log("更新了数据库", res);
@@ -92,27 +96,17 @@ Page({
       navBarHeight: app.globalData.navBarHeight,
     });
     // 接收其他页面传值-------------------------
-    this.setData({ id: options.id });
-    db.where({ _id: this.data.id }).get({
+    db.where({ _id: app.globalData.id }).get({
       success: (res) => {
+        this.setData({ Curriculum: res.data[0] });
         let CourseList = [];
         for (let i in res.data[0].Course) {
           res.data[0].Course[i].name = i;
           CourseList.push(res.data[0].Course[i]);
         }
         this.setData({ CourseList });
-        this.setData({ CourseListBackup: CourseList });
-        console.log("当前课程表有这些课程", this.data.CourseList);
+        console.log("当前课程表有这些课程", res, this.data.CourseList);
       },
     });
-  },
-  /* 生命周期函数--监听页面卸载*/
-  onUnload() {
-    if (
-      JSON.stringify(this.data.CourseList) !=
-      JSON.stringify(this.data.CourseListBackup)
-    ) {
-      console.log("有更改");
-    }
   },
 });
